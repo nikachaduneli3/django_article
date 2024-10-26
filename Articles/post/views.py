31692f54494a4166b95e4f31885ed99d55f953a1
nikachaduneli <chaduneli@hmg.software>
2024-10-26 15:15:12 +0400

    add comments

--- Articles/post/views.py
+++ Articles/post/views.py
@@ -6,36 +6,34 @@ from .forms import PostForm
 def home(request):
 
     posts = Post.objects.all() #ყველა პოსტის წამოღება
-    context = {'posts': posts}
 
-    return render(request, 'template_1.html', context=context)  
+    return render(request, 'template_1.html', context={'posts': posts})  
 
 
 def detail_post(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+    post = get_object_or_404(Post, pk=pk) # pk(id) -ით ბაზიდან პოსტის წამოღება
     return render(request, 'detail_post.html', context={'post': post})
 
 
 def create_post(request):
-    postfrom = PostForm()
     if request.method == 'POST':
         postfrom = PostForm(request.POST) # PostForm-ის შევსება request-ში მოსული მონაცემებით
         if postfrom.is_valid(): # მონაცემების ვალიდურობის შემოწმება
             postfrom.save() # ჩანაწერის ბაზაში შენახვა
             return redirect('home') 
-    return render(request, 'create_post.html', context={'form': postfrom})
+    return render(request, 'create_post.html', context={'form': PostForm()})
 
 
 def update_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
 
-    postfrom = PostForm(instance=post)
     if request.method == 'POST':
-        postfrom = PostForm(request.POST, instance=post) # PostForm-ის შევსება request-ში მოსული მონაცემებით
+        postfrom = PostForm(request.POST, instance=post) # PostForm-ს თუ instance-ად გადაცემ შენ ობიექს ახლის შექმნი მაგოვრად იმ შენს გადაცემულ ობიუექტს განაახლებს
         if postfrom.is_valid(): # მონაცემების ვალიდურობის შემოწმება
             postfrom.save() # ჩანაწერის ბაზაში შენახვა
             return redirect('home') 
-    return render(request, 'update_post.html', context={'form': postfrom})
+    return render(request, 'update_post.html', 
+                  context={'form': PostForm(instance=post)}) # PostForm-ს instance-ად გადააწოდე შენი პოსტი და ფორმაში ველები უკვე შევსებული იქნება მონაცემებით
 
 
 def delete_post(request, pk):
